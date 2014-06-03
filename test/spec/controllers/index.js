7ea3823f57a40eb1ea1ebfb66e562b76c4531b8d
Prabin Varma <prabinv@gmail.com>
2014-06-03 14:56:26 -0500

    Added cache busting token to config api call

--- test/spec/controllers/index.js
+++ test/spec/controllers/index.js
@@ -5,17 +5,58 @@ describe('Controller: IndexCtrl', function() {
 
   beforeEach(module('volusion.controllers'));
 
+  var api;
   var controller;
   var scope;
   var rootScope;
+  var tokenGenerator;
 
-  beforeEach(inject(function($controller, $rootScope) {
+  var cacheBustingToken = { 'foo': 'bar' };
+
+  var response = {
+    data: {
+      seo: {
+        foo: 'bar',
+        baz: 'qux'
+      },
+      checkout: {
+        cartId: '1234'
+      }
+    }
+  };
+
+  /*jshint camelcase: false */
+  beforeEach(inject(function($controller, $rootScope, _api_, _tokenGenerator_) {
+    api = _api_;
     rootScope = $rootScope;
     scope = $rootScope.$new();
+
+    tokenGenerator = _tokenGenerator_;
+
+    sinon.stub(tokenGenerator, 'getCacheBustingToken', function() {
+      return cacheBustingToken;
+    });
+
+    sinon.stub(api.config, 'get', function() {
+      return {
+        then: function(fn) {
+          return fn(response);
+        }
+      };
+    });
+
     controller = $controller('IndexCtrl', {
       $scope: scope
     });
+
   }));
+
+  afterEach(function() {
+    api.config.get.restore();
+    tokenGenerator.getCacheBustingToken.restore();
+  });
+
+  /*jshint camelcase: true */
 
   it('should navigate home when no sub-route is defined', function() {
     inject(function($state) {
@@ -25,6 +66,10 @@ describe('Controller: IndexCtrl', function() {
       expect(go).to.have.been.calledWithExactly('.home', null, { location: 'replace' });
       $state.go.restore();
     });
+  });
+
+  it('sends the cachebusting token to the config api call', function () {
+    expect(api.config.get).to.have.been.calledWithExactly(cacheBustingToken);
   });
 
 });
